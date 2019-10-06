f19f7a136697ce4b1ebad5c0bed62eba93e85cc9
changjun89 <leechang0423@naver.com>
2019-10-06 23:50:43 +0900

    feature "copy region controller,service from adminApi to customerApi "

---
+++ customerApi/src/main/java/me/changjun/restaurant/interfaces/RegionController.java
@@ -0,0 +1,28 @@
+package me.changjun.restaurant.interfaces;
+
+import me.changjun.restaurant.application.RegionService;
+import me.changjun.restaurant.domain.Region;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@RestController
+public class RegionController {
+
+    private final RegionService regionService;
+
+    public RegionController(RegionService regionService) {
+        this.regionService = regionService;
+    }
+
+    @GetMapping("/api/regions")
+    public List<Region> getRegions() {
+        return regionService.getRegions();
+    }
+}
