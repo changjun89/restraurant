ae6db3e034f491ebacd2fb7a7970a10fb3049479
changjun89 <leechang0423@naver.com>
2019-10-10 21:58:05 +0900

    feat "customerApi user password encrypt"

--- customerApi/src/main/java/me/changjun/restaurant/application/UserService.java
+++ customerApi/src/main/java/me/changjun/restaurant/application/UserService.java
@@ -2,6 +2,8 @@ package me.changjun.restaurant.application;
 
 import me.changjun.restaurant.domain.User;
 import me.changjun.restaurant.domain.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -15,10 +17,12 @@ public class UserService {
     }
 
     public User create(String email, String name, String password) {
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String encodedPassword = passwordEncoder.encode(password);
         User user = User.builder()
                 .name(name)
                 .email(email)
-                .password(password)
+                .password(encodedPassword)
                 .build();
 
         return userRepository.save(user);
