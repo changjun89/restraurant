e6385cc080a1ece461774d68b400033801a3dcfc
changjun89 <leechang0423@naver.com>
2019-10-13 11:22:41 +0900

    feat "jwtAuthentication filter add"

--- common/src/main/java/me/changjun/utils/JwtUtil.java
+++ common/src/main/java/me/changjun/utils/JwtUtil.java
@@ -1,5 +1,6 @@
 package me.changjun.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -22,5 +23,12 @@ public class JwtUtil {
                 .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
         return token;
+    }
+
+    public Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(key)
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
--- common/src/test/java/me/changjun/utils/JwtUtilTest.java
+++ common/src/test/java/me/changjun/utils/JwtUtilTest.java
@@ -1,17 +1,35 @@
 package me.changjun.utils;
 
+import io.jsonwebtoken.Claims;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JwtUtilTest {
 
+    private static final String SECRET = "12345678901234567890123456789012";
+
+    private JwtUtil jwtUtil;
+
+    @Before
+    public void setup() {
+        jwtUtil = new JwtUtil(SECRET);
+    }
+
     @Test
     public void createToken() {
-        String secret = "12345678901234567890123456789012";
-        JwtUtil jwtUtil = new JwtUtil(secret);
-        String token = jwtUtil.createToken(1L,"Changjun");
+        String token = jwtUtil.createToken(1L, "Changjun");
 
         assertThat(token).contains(".");
+    }
+
+    @Test
+    public void getClaims() {
+        String token = "eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsIm5hbWUiOiJDaGFuZ2p1biJ9.9y_qfcOH9PlX6942ajVOpMs6ateGYL3OCuyUU9-uByw";
+        Claims claims = jwtUtil.getClaims(token);
+
+        assertThat(claims.get("name")).isEqualTo("Changjun");
+        assertThat(claims.get("userId", Long.class)).isEqualTo(1L);
     }
 }
